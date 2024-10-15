c5f32c6020bc987690e60f488e293efd8f874980
Stilllee <orodae@gmail.com>
2024-10-15 20:17:28 +0900

    [feat] 서치 바 기능 구현
    - 검색어 입력 후 'Enter' 혹은 검색 버튼 클릭시 검색페이지로 이동
    - 현재의 쿼리 스트링 q의 값과 input값 동기화
    - 같은 단어로 여러번 검색 방지

--- src/app/(with-header)/(with-search-bar)/search/page.tsx
+++ src/app/(with-header)/(with-search-bar)/search/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
-  return <div>search page</div>;
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { q?: string };
+}) {
+  return <div>검색 결과 : {searchParams.q}</div>;
 }
--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,13 +1,47 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
 export default function SearchBar() {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const q = searchParams.get("q");
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") onSubmit();
+  };
+
   return (
     <div className="flex gap-4" role="search">
       <input
-        className="border-lightGray placeholder:text-gray h-12 w-full items-center rounded-[4px] border px-3 py-3 text-sm text-textBlack outline-none focus:border-main"
+        className="h-12 w-full items-center rounded-[4px] border border-lightGray px-3 py-3 text-sm text-textBlack outline-none placeholder:text-gray focus:border-main"
         type="search"
         placeholder="레시피를 검색해보세요!"
         autoComplete="off"
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDown}
       />
-      <button className="h-12 min-w-24 rounded-[4px] border border-main bg-main px-3 text-sm font-bold text-white">
+      <button
+        className="h-12 min-w-24 rounded-[4px] border border-main bg-main px-3 text-sm font-bold text-white"
+        onClick={onSubmit}
+      >
         검색
       </button>
     </div>
