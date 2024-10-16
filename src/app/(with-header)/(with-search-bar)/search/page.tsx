04da0d6a7489956e42b4f3305dc297131c77079f
Stilllee <orodae@gmail.com>
2024-10-16 16:58:09 +0900

    [feat] 서치 페이지 UI 구현

--- src/app/(with-header)/(with-search-bar)/search/page.tsx
+++ src/app/(with-header)/(with-search-bar)/search/page.tsx
@@ -1,7 +1,25 @@
+import RecipeItem from "@/app/components/RecipeItem";
+import recipes from "@/mock/recipes.json";
+
 export default function Page({
   searchParams,
 }: {
   searchParams: { q?: string };
 }) {
-  return <div>검색 결과 : {searchParams.q}</div>;
+  const filteredRecipes = recipes.filter((recipe) =>
+    recipe.title.includes(searchParams.q as string),
+  );
+
+  return (
+    <>
+      <h2 className="sr-only">레시피 검색 결과</h2>
+      <ul className="my-4 grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {filteredRecipes.map((recipe) => (
+          <li key={recipe.id}>
+            <RecipeItem {...recipe} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
