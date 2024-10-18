343b278e80c98967be6aa302ff3a38ed72f46769
Stilllee <orodae@gmail.com>
2024-10-18 14:36:03 +0900

    [feat] 인덱스 페이지 api 연결
    - 전체 레시피 데이터 페칭 및 캐싱

--- src/app/(with-header)/(with-search-bar)/page.tsx
+++ src/app/(with-header)/(with-search-bar)/page.tsx
@@ -1,17 +1,30 @@
+import { RecipeData } from "@/types";
 import RecipeItem from "@/app/components/RecipeItem";
-import recipes from "@/mock/recipes.json";
+
+async function AllRecipes() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/recipe`, {
+    cache: "force-cache",
+  });
+  if (!res.ok) return <p>오류가 발생했습니다.</p>;
+
+  const allRecipes: RecipeData[] = await res.json();
+
+  return (
+    <ul className="my-4 grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {allRecipes.map((recipe) => (
+        <li key={recipe.id}>
+          <RecipeItem {...recipe} />
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default function Home() {
   return (
     <>
       <h2 className="sr-only">레시피 목록</h2>
-      <ul className="my-4 grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {recipes.map((recipe) => (
-          <li key={recipe.id}>
-            <RecipeItem {...recipe} />
-          </li>
-        ))}
-      </ul>
+      <AllRecipes />
     </>
   );
 }
