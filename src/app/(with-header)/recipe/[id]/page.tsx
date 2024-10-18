ff1f753e218fefdec07e50d1081f612b97c9059b
Stilllee <orodae@gmail.com>
2024-10-18 15:05:35 +0900

    [feat] 상세 페이지 api 연결
    - 개별 레시피 상세 데이터 페칭 및 캐싱

--- src/app/(with-header)/recipe/[id]/page.tsx
+++ src/app/(with-header)/recipe/[id]/page.tsx
@@ -1,21 +1,18 @@
 import IngredientItem from "@/app/components/IngredientItem";
 import { RecipeData } from "@/types";
 
-const recipe: RecipeData = {
-  id: 2,
-  title: "소고기 미역국",
-  servings: 4,
-  ingredient: ["소고기", "미역", "멸치액젓", "다진마늘", "국간장", "참기름"],
-  directions:
-    "1. 미역 10g을 불린다.\n2. 참기름 2T, 소고기 100g을 중약불에서 볶는다.\n3. 소고기가 익으면 불린 미역을 넣어 볶는다.\n4. 국간장 3T 넣어 볶고 물 1.3L를 넣어 끓인다.\n5. 물이 끓으면 다진마늘을 넣고, 마지막에 멸치액젓 1과1/2T를 넣어 간을 맞춘다.",
-};
+export default async function Page({ params }: { params: { id: string } }) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/recipe/${params.id}`,
+    { cache: "force-cache" },
+  );
+  if (!res.ok) <p>오류가 발생했습니다.</p>;
 
-export default function Page({ params }: { params: { id: string } }) {
-  const { title, servings, ingredient, directions } = recipe;
+  const { title, servings, ingredient, directions }: RecipeData =
+    await res.json();
 
   return (
     <article className="py-3">
-      {/* {params.id} recipe detail page */}
       <h2 className="text-2xl font-bold">{title}</h2>
       <span className="my-4 inline-block rounded-full bg-main px-2 py-1 text-sm font-bold text-white">
         {servings}인분
@@ -31,7 +28,7 @@ export default function Page({ params }: { params: { id: string } }) {
       <hr className="mb-4 mt-6 border-lightGray" />
       <section>
         <h3 className="mb-2 text-lg font-semibold">조리 방법</h3>
-        <p className="text-textGray whitespace-pre-line leading-8">
+        <p className="whitespace-pre-line leading-8 text-textGray">
           {directions}
         </p>
       </section>
