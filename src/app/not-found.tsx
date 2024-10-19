9a8511fc714fbe944fdfb14066646ef87079368f
Stilllee <orodae@gmail.com>
2024-10-19 17:59:31 +0900

    [feat] 404 페이지 구현

---
+++ src/app/not-found.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function NotFound() {
+  const router = useRouter();
+
+  return (
+    <main className="flex h-screen flex-col items-center justify-center gap-16 px-8 py-5">
+      <h1 className="sr-only">404 - Page Not Found</h1>
+      <span
+        className="block text-center text-9xl font-bold text-main"
+        aria-hidden
+      >
+        404
+      </span>
+      <div className="flex flex-col items-center justify-center gap-9">
+        <p className="text-3xl sm:text-4xl">페이지를 찾을 수 없어요.</p>
+        <p className="max-w-lg text-center text-base sm:text-lg">
+          페이지 주소를 다시 한번 확인해 주세요.
+        </p>
+        <button
+          className="w-60 rounded-xl bg-main py-3 text-center text-white"
+          onClick={() => router.replace("/")}
+        >
+          테이스토리지 홈
+        </button>
+      </div>
+    </main>
+  );
+}
