a33096ece1bb457e50da970f58854c99b198eac4
Stilllee <orodae@gmail.com>
2024-10-31 14:43:59 +0900

    [fix] 텍스트 수정
    - 레시피 작성하기 -> 레시피 작성

--- src/app/(with-header)/new/page.tsx
+++ src/app/(with-header)/new/page.tsx
@@ -3,7 +3,7 @@ import RecipeForm from "../../components/RecipeForm";
 export default function Page() {
   return (
     <>
-      <h2 className="sr-only">레시피 작성하기</h2>
+      <h2 className="sr-only">레시피 작성</h2>
       <RecipeForm />;
     </>
   );
